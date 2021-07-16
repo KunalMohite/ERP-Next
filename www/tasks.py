3444abb6ad9e2ed69b7c5c79d1a5395fa268de07
KunalMohite <kunalmohite56@gmail.com>
2021-07-16 14:01:43 +0530

    hooks commit

---
+++ www/tasks.py
@@ -0,0 +1,16 @@
+import frappe
+import string
+import random
+
+def all():
+    pass
+
+def cron():
+    letters = string.ascii_letters
+    note = "".join(random.choice(letters) for i in range(20))
+    
+    new_note = frappe.get_doc({"doctype":"Note",
+    "title": note                 
+        })
+    new_note.insert()
+    frappe.db.commit()
